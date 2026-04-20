996054b2ac0f44eacf88b05997b97ad7a5391e9a
kofta999 <mostafaxxx555@gmail.com>
2026-04-20 21:37:19 +0200

    init

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { hostname } from "os";
+const app = new Hono();
+app.get("/", (c) => {
+    return c.text(`Hello from ${hostname()}!`);
+});
+serve({
+    fetch: app.fetch,
+    port: 8080,
+}, (info) => {
+    console.log(`Server is running on http://${hostname()}:${info.port}`);
+});
